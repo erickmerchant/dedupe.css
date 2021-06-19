120dd68f117620441f5c237978aa67b704a93c00
Erick Merchant <erickmerchant@gmail.com>
2021-06-18 20:43:17 -0400

    Change how class lists can be constructed

--- css.js
+++ css.js
@@ -2,6 +2,8 @@ export const RAW = Symbol('raw')
 
 export const REPLACEMENT = Symbol('replacement')
 
+export const VALIDATE = Symbol('validate')
+
 export const css = (strs, ...vars) => {
   const result = {[RAW]: {strs, vars}}
 
@@ -14,4 +16,44 @@ export const css = (strs, ...vars) => {
       }
     }
   })
+}
+
+export const define = (vmapped) => {
+  const result = {}
+
+  if (vmapped) {
+    result[VALIDATE] = (primary, list) => {
+      return list.reduce((acc, curr) => {
+        if (!vmapped[primary].includes(curr)) return false
+
+        return acc
+      }, true)
+    }
+  }
+
+  return result
+}
+
+export const concat = (classes, name, props) => {
+  let list = []
+
+  for (const [key, val] of Object.entries(props)) {
+    if (val === true) {
+      list.push(key)
+    } else if (val) {
+      list.push(`${key}${val}`)
+    }
+  }
+
+  if (import.meta.env?.DEV) {
+    if (!classes[VALIDATE](name, list)) {
+      throw Error(`invalid classList: ${list.join(' ')}`)
+    }
+  }
+
+  list = list.map((item) => classes[item])
+
+  list.unshift(classes[name])
+
+  return list.join(' ')
 }
